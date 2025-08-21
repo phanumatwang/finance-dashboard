2478f6f9b43786cb1a75022b119cd61862b2ac33
phanumatwang <160455292+phanumatwang@users.noreply.github.com>
2025-08-21 15:12:44 +0700

    Auto commit

--- push.bat
+++ push.bat
@@ -6,7 +6,7 @@ set COMMIT_MSG=%*
 
 if "%COMMIT_MSG%"=="" set COMMIT_MSG=Auto commit & deploy
 
-echo 🚀 กำลัง Add ไฟล์ทั้งหมด...
+
 git add .
 
 echo 📝 Commit: %COMMIT_MSG%
@@ -15,6 +15,6 @@ git commit -m "%COMMIT_MSG%"
 echo ⬆️ Push ขึ้น GitHub...
 git push origin main
 
-echo ✅ เสร็จแล้ว! ถ้าผูกกับ Vercel จะ Deploy อัตโนมัติ 🎉
+
 endlocal
-npm run deploy -- "fix bug in customer list modal"
+
